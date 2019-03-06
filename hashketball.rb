2ee373db43e5afd621f711f77ceaba910254fb3b
A Belgrave <abelgrave95@gmail.com>
2019-03-06 15:39:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,13 +2,13 @@
 def game_hash
   match_hash = {
     home: {
-      team_name: "Brooklyn Nets", colors: "Black, White", 
+      team_name: "Brooklyn Nets", colors: "Black, White",
       players: {
-        
+
       }
     },
     away: {
-      team_name: "Charlotte Hornets", colors: "Turquoise, Purple", 
+      team_name: "Charlotte Hornets", colors: "Turquoise, Purple",
       players: {
         "Alan Anderson" => {
           :number => 0,
