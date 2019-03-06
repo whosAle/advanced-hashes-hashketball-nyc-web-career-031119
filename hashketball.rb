88ff0ca19b89491344b0b3dad2a8eede52aae8f6
A Belgrave <abelgrave95@gmail.com>
2019-03-06 15:32:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,43 @@
 # Write your code here!
+def game_hash
+  match_hash = {
+    home: {
+      
+    },
+    away: {
+      
+    }
+  }
+  
+end
 
+def num_points_scored
+  
+end
 
+def shoe_size
+  
+end
 
+def team_colors(team)
+  
+end
+
+def team_names(team)
+  
+end
+
+def player_numbers(player)
+  
+end
+
+def player_stats(player)
+  
+end
+
+def big_shoe_rebounds
+  
+end
 
 
 
